f8628a619d3781aed34ebe3fadb269a93f9d4403
Allenxuxu <120582243@qq.com>
2019-11-07 19:16:18 +0800

    update example test

--- example_test.go
+++ example_test.go
@@ -11,7 +11,34 @@ func ExampleRingBuffer() {
 
 	_, _ = rb.Read(buf)
 	fmt.Println(string(buf))
+
+	rb.Write([]byte("1234567890"))
+	rb.VirtualRead(buf)
+	fmt.Println(string(buf))
+	fmt.Println(rb.Length())
+	fmt.Println(rb.VirtualLength())
+	rb.VirtualFlush()
+	fmt.Println(rb.Length())
+	fmt.Println(rb.VirtualLength())
+
+	rb.VirtualRead(buf)
+	fmt.Println(string(buf))
+	fmt.Println(rb.Length())
+	fmt.Println(rb.VirtualLength())
+	rb.VirtualRevert()
+	fmt.Println(rb.Length())
+	fmt.Println(rb.VirtualLength())
 	// Output: 4
 	// 1020
 	// abcd
+	// 1234
+	// 10
+	// 6
+	// 6
+	// 6
+	// 5678
+	// 6
+	// 2
+	// 6
+	// 6
 }
